f4bca921a46320ac449502efcb890d1a3b613a20
Mike <pection.naphat@gmail.com>
2020-09-14 06:57:29 +0700

    Fuck

--- SaveImageFromweb/ExtractImage.py
+++ SaveImageFromweb/ExtractImage.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 from PIL import Image
 num=1
-path ="/Users/pection/Documents/mn_furniture/AddwatermarkProgram/imagess/"
+path ="/Users/pection/Documents/FASTCat/media/"
 #we shall store all the file names in this list
 filelist=[]
 for root, dirs, files in os.walk(path):
@@ -11,19 +11,19 @@ for root, dirs, files in os.walk(path):
         if(file.endswith(".jpg")):
             filelist.append(os.path.join(root,file))
 print (filelist)
-logo=Image.open('logo.png')
-logo2=Image.open('logo2.png')
-watermark = Image.open('WatermarkB5.png')
-watermark2 = Image.open('WatermarkB3.png')
-
-logoWidth = watermark.width
-logoHeight = watermark.height
-watermarkW=watermark.width
-watermarkH=watermark.height
-logo2Width = watermark2.width
-logo2Height = watermark2.height
+# logo=Image.open('logo.png')
+# logo2=Image.open('logo2.png')
+# watermark = Image.open('WatermarkB5.png')
+# watermark2 = Image.open('WatermarkB3.png')
+#
+# logoWidth = watermark.width
+# logoHeight = watermark.height
+# watermarkW=watermark.width
+# watermarkH=watermark.height
+# logo2Width = watermark2.width
+# logo2Height = watermark2.height
 for filename in filelist:
-    # image = Image.open(filename)
+    image = Image.open(filename)
     # imageWidth = image.width
     # imageHeight = image.height
     # if imageWidth<500 :
@@ -41,5 +41,5 @@ for filename in filelist:
     #     image2=image.copy()
     #     image2.paste(watermark,(int((img_w-logoWidth)/2),int((img_h-logoHeight)/2)),watermark)
     num += 1
-    image2.save(filename)
-    # image.save('/Users/pection/Documents/mn_furniture/AddwatermarkProgram/New/'+str(num)+'.png')
+    # image2.save(filename)
+    image.save('/Users/pection/Documents/FASTCat/ExtractImage/'+str(num)+'.png')
