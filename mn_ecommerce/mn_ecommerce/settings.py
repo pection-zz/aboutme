b28b26ac7ed03c75da1a06da93b0d14dc9a3158d
Mike <pection.naphat@gmail.com>
2020-09-16 08:51:08 +0700

    update new

--- mn_ecommerce/mn_ecommerce/settings.py
+++ mn_ecommerce/mn_ecommerce/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'crispy_forms',
+    'django_countries',
+    'mn_web'
 ]
 
 MIDDLEWARE = [
@@ -117,4 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
--- mn_ecommerce/mn_web/views.py
+++ mn_ecommerce/mn_web/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, UpdateView,View
+# from django.core.urlresolvers import reverse
+# from contacts.models import Contact
+# import forms
 # Create your views here.
+import random
+import string
+import stripe
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+class HomeView(View):
+    template_name = "index.html"
