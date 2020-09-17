6c13c81ed410bf934c84c2ee0ca218d99be48d42
Mike <pection.naphat@gmail.com>
2020-09-17 09:31:08 +0700

    update new

---
+++ MNfurniture/Program_python/select_all.py
@@ -0,0 +1,14 @@
+import sqlite3 as lite
+
+con = lite.connect('test.db')
+
+with con:
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM cars")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print (row)
+
