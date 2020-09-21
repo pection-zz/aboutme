e74fba9c1c018c0fde573124e7eaac364c2fdc0b
Mike <pection.naphat@gmail.com>
2020-09-21 15:09:19 +0700

    Update Ecommerce

--- mn_ecommerce/mnEcom/settings.py
+++ mn_ecommerce/mnEcom/settings.py
@@ -4,8 +4,9 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-# SECRET_KEY = 'm^+d1v^f-6%0#bzoi94z^boygo5x+a9obhkkbhegr7zqb7i^+4'
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
 
 ALLOWED_HOSTS = []
 
