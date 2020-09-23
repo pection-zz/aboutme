a6db5d6bb218784a502d6f86c8559f00210f8b20
Mike <pection.naphat@gmail.com>
2020-09-23 09:11:40 +0700

    Update Program

--- MNfurniture/Program_python/creategui.py
+++ MNfurniture/Program_python/creategui.py
@@ -1,37 +1,33 @@
 import PySimpleGUI as sg
 
 sg.theme('DarkAmber')   # Add a touch of color
-layoutdata =[]
+
+
 # All the stuff inside your window.
-Datatemplate=['Name','Phone','Address','Order','Delivery','Price','Value','Discount','Deposit','Remain']
-for i in range(len(Datatemplate)):
-    layoutdata.append([sg.Text(Datatemplate[i]), sg.InputText()])
-layoutdata.append([sg.Button('Ok'), sg.Button('Cancel')])
+Data_template=['PhotoPath','Name','Phone','Address','Order','Delivery','Price','Value','Discount','Deposit','Remain']
+def CreateTablegui(Datatemplate):
+    layoutdata = []
+    for i in range(len(Datatemplate)):
+        layoutdata.append([sg.Text(Datatemplate[i]), sg.InputText()])
+    layoutdata.append([sg.Button('Ok'), sg.Button('Cancel')])
+    return layoutdata
+def changedicttolist(listofdict):
+    listvalues=[]
+    for valuefrombill in listofdict.values():
+        listvalues.append(valuefrombill)
+    return listvalues
 
-window = sg.Window('Billing Address',layoutdata)
+# layout=CreateTablegui(Data_template)
+# window = sg.Window('Billing Address',layout)
 # Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    window.Element('_LISTBOX_').Update(values=Datatemplate)
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-
-    # for key , value in values.iteritems():
-        # temp =[key,value]
-        # dictlist.append(value)
-# window.Close()
-    Data = dict(zip(Datatemplate,values))
-    print (values)
-#     Data ={ 'Name': values[0],
-#     'Phone':values[1],
-#     'Address':values[2],
-#     'Order': values[3],
-#     'Delivery':values[4],
-#     'Price':values[5],
-#     'Discount':values[6],
-#     'Deposit':values[7],
-#     'Remain':values[8]
-#     }
-#     for datas in Data:
-#         print(datas)
-window.close()
+# while True:
+#     event, values = window.read()
+# #    window.Element('_LISTBOX_').Update(values=Datatemplate)
+#     if event == sg.WIN_CLOSED or event == 'Cancel' or event == 'Ok': # if user closes window or clicks cancel
+#         window.close()
+#         break
+#
+#
+# listofvalues=changedicttolist(values)
+# Data = dict(zip(Data_template,listofvalues))
+# print (Data)
--- MNfurniture/Program_python/resizefill.py
+++ MNfurniture/Program_python/resizefill.py
@@ -1,25 +1,8 @@
 import cv2
 import argparse
 import tldextract
-from PIL import ImageFont, ImageDraw, Image  
+from PIL import ImageFont, ImageDraw, Image
 import PySimpleGUI as sg
-
-broken_images = []
-image_urls=[]
-parser = argparse.ArgumentParser(description="This is Resize image  script")
-parser.add_argument("-i","--in",action="store",dest="input",default=None,help="<Required>image path",required=True)
-parser.add_argument("-o","--out",action="store",dest="output",default="/Users/pection/Programing/aboutme/MNfurniture/Bill/PretoPaid/Pretopaid.jpg",help="<Required>image path",required=False)
-parser.add_argument("-wi","--width",action="store",dest="width",default=200,help="<Required>image path",required=False)
-parser.add_argument("-hi","--height",action="store",dest="height",default=200,help="<Required>image path",required=False)
-results = parser.parse_args()
-xyaxis={"x_number":350,"x_price":400,"x_promotion":450,"x_priceafterpromotion":520 ,"x_summary":520,"x_deposit":520,"x_remain":520,"y_number":450,"y_price":450,"y_promotion":450,"y_priceafterpromotion":450,"y_summary":660,"y_deposit":700,"y_remain":750}
-x_offset=100
-y_offset=350
-imgpath = results.input
-savepath= results.output
-width_image=results.width
-height_image=results.height
-
 def resize(image, width = None, height = None, inter = cv2.INTER_AREA):
     # initialize the dimensions of the image to be resized and grab the image size
     dim = None
@@ -41,24 +24,33 @@ def resize(image, width = None, height = None, inter = cv2.INTER_AREA):
     resized = cv2.resize(image, dim, interpolation = inter)
     # return the resized image
     return resized
-
-imgdefault=cv2.imread(imgpath)
-img = resize(image=imgdefault,width=int(width_image),height=int(height_image))
-template=cv2.imread("/Users/pection/Programing/aboutme/MNfurniture/Bill/Template/TemplateBill.jpg")
-print(width_image,height_image)
-
-template[y_offset:y_offset+img.shape[0], x_offset:x_offset+img.shape[1]] = img
-# cv2.putText(
-#      template, 
-#      "1", 
-#      (xyaxis["x_number"],xyaxis["y_number"]),
-#      cv2.FONT_HERSHEY_SIMPLEX, 
-#      1, (0, 0, 0, 255), 3) 
-cv2.imwrite(savepath,template)
-image = Image.open(savepath)  
-draw = ImageDraw.Draw(image)  
-# use a truetype font  
-font = ImageFont.truetype("/System/Library/Fonts/HelveticaNeue.ttc", 13)  
-draw.text((xyaxis["x_number"],xyaxis["y_number"]), "1", font=font,fill=(0,0,0,0))  
-image.save("/Users/pection/Programing/aboutme/MNfurniture/Bill/PretoPaid/Pretopaid2.jpg")  
-
+broken_images = []
+image_urls=[]
+# parser = argparse.ArgumentParser(description="This is Resize image  script")
+# parser.add_argument("-i","--in",action="store",dest="input",default=None,help="<Required>image path",required=True)
+# parser.add_argument("-o","--out",action="store",dest="output",default="/Users/pection/Programing/aboutme/MNfurniture/Bill/PretoPaid/Pretopaid.jpg",help="<Required>image path",required=False)
+# parser.add_argument("-wi","--width",action="store",dest="width",default=200,help="<Required>image path",required=False)
+# parser.add_argument("-hi","--height",action="store",dest="height",default=200,help="<Required>image path",required=False)
+# results = parser.parse_args()
+xyaxis={"x_value":350,"x_price":400,"x_discount":450,"x_priceafterdiscount":520 ,"x_summary":520,"x_deposit":520,"x_remain":520,"y_value":450,"y_price":450,"y_discount":450,"y_priceafterdiscount":450,"y_summary":660,"y_deposit":700,"y_remain":750}
+x_offset=100
+y_offset=350
+# imgpath = results.input
+# savepath= results.output
+# width_image=results.width
+# height_image=results.height
+#
+#
+#
+# imgdefault=cv2.imread(imgpath)
+# img = resize(image=imgdefault,width=int(width_image),height=int(height_image))
+# template=cv2.imread("/Users/pection/Programing/aboutme/MNfurniture/Bill/Template/TemplateBill.jpg")
+# template[y_offset:y_offset+img.shape[0], x_offset:x_offset+img.shape[1]] = img
+#
+# cv2.imwrite(savepath,template)
+# image = Image.open(savepath)
+# draw = ImageDraw.Draw(image)
+# # use a truetype font
+# font = ImageFont.truetype("/System/Library/Fonts/HelveticaNeue.ttc", 13)
+# draw.text((xyaxis["x_number"],xyaxis["y_number"]), "1", font=font,fill=(0,0,0,0))
+# image.save("/Users/pection/Programing/aboutme/MNfurniture/Bill/PretoPaid/Pretopaid2.jpg")
