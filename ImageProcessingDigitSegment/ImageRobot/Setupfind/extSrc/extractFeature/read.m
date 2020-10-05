74511b660fa6a492b7cdc74ac2973a2aa8777119
Mike <pection.naphat@gmail.com>
2020-10-06 00:26:39 +0700

    Change Directory

--- ImageProcessingDigitSegment/ImageRobot/Setupfind/extSrc/extractFeature/read.m
+++
@@ -1,7 +0,0 @@
-fid = fopen('HOGFull1Font-imagesize-50-block-20-cell-20-bin-9-sined-True-nimage-30000.csv');
-out = textscan(fid,'%s%f%f','delimiter',',');
-fclose(fid);
-
-date = datevec(out{1});
-col1 = out{2};
-col2 = out{3};
